58f3ec7321d46e1e0472a5953ac4035adfd76aa4
johncbogil <johncbogil@gmail.com>
2015-03-25 13:01:51 -0400

    merge

---
+++ Location/Keys.h
@@ -0,0 +1,17 @@
+//
+//  Keys.h
+//  Voices
+//
+//  Created by Aditya Narayan on 3/25/15.
+//  Copyright (c) 2015 John Bogil. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Keys : NSObject
+
+#define SFKEY @"1"
+#define GOOGKEY @"1"
+
+
+@end
