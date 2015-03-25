09c9b921bc8169fd2d6133f17a0e23bec51b7784
johncbogil <johncbogil@gmail.com>
2015-03-25 11:52:43 -0400

    test

---
+++ Location/Location/Keys.h
@@ -0,0 +1,17 @@
+//
+//  Keys.h
+//  Voices
+//
+//  Created by Aditya Narayan on 3/24/15.
+//  Copyright (c) 2015 John Bogil. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Keys : NSObject
+
+#define SFKEY 
+#define GOOGMAPSKEY
+#define GOOGCIVKEY
+
+@end
