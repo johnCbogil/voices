5ce0a2cbb747d82c629ed48f288a56247f7d7deb
johncbogil <johncbogil@gmail.com>
2015-04-22 19:40:26 -0400

    Moved tableView delegat methods into it's own class

---
+++ Location/Location/HomeTableView.h
@@ -0,0 +1,13 @@
+//
+//  HomeTableView.h
+//  Voices
+//
+//  Created by Aditya Narayan on 4/22/15.
+//  Copyright (c) 2015 John Bogil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeTableView : UITableView
+
+@end
---
+++ Location/Location/HomeTableView.m
@@ -0,0 +1,21 @@
+//
+//  HomeTableView.m
+//  Voices
+//
+//  Created by Aditya Narayan on 4/22/15.
+//  Copyright (c) 2015 John Bogil. All rights reserved.
+//
+
+#import "HomeTableView.h"
+
+@implementation HomeTableView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
